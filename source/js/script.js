f19872885af715ad352a6aff2884fff19d2c5a4a
Анна Кузнецова <akimova_anna_25@mail.ru>
2019-12-14 00:29:00 +0300

    Исправление опечатки.

--- source/js/script.js
+++ source/js/script.js
@@ -34,7 +34,7 @@ if (profileForm) {
   var storageCatWeight = '';
   var storageCatAge = '';
   var storageOwnerEmail = '';
-  var storageOwnerTtel = '';
+  var storageOwnerTel = '';
 
   try {
     storageCatName = localStorage.getItem('catName');
