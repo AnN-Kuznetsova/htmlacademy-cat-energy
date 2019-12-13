295229b76f1b9056026f488ecae4436daf46c604
Анна Кузнецова <akimova_anna_25@mail.ru>
2019-12-13 14:59:15 +0300

    Чиним работу main-nav на JS.

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,8 @@ var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
+navMain.classList.add('main-nav--closed');
+navMain.classList.remove('main-nav--opened');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
