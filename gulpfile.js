757383dd64e0059293d1c73a262286e0622f6b4a
Анна Кузнецова <akimova_anna_25@mail.ru>
2019-12-23 23:54:07 +0300

    Исправление копирования картинок.

--- gulpfile.js
+++ gulpfile.js
@@ -111,7 +111,7 @@ gulp.task("html", function () {
 gulp.task("copy", function () {
   return gulp.src([
       "source/fonts/**/*.{woff,woff2}",
-      "source/img/**",
+      "source/img/**/*.{png,jpg,svg}",
       "source/*.ico"
     ], {
       base: "source"
